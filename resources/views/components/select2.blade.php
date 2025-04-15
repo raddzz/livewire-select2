463091898cd672caaac7f1e1ab784f8800e02139
James Radley <james@jamesradley.net>
2025-04-15 11:01:48 +0100

    Update select2.blade.php

--- resources/views/components/select2.blade.php
+++ resources/views/components/select2.blade.php
@@ -1,27 +1,28 @@
-
-
 <div wire:ignore>
-    <select class="select2-{{$this->id}} {{$this->class ?? ''}}" @if($this->multiple) multiple="multiple" @endif >
+    <select class="select2-{{ $this->id }} {{ $this->class ?? '' }}" @if($this->multiple) multiple="multiple" @endif>
         @foreach($this->options as $key => $option)
-            <option value="{{$key}}" @if($key == $this->model) selected @endif>{{$option}}</option>
+            <option value="{{ $key }}" @if($this->multiple ? in_array($key, (array)$this->model) : $key == $this->model) selected @endif>
+                {{ $option }}
+            </option>
         @endforeach
     </select>
 </div>
 
-
-
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        prepareSelect2()
+        initSelect2()
+
         document.addEventListener('livewire-select2-init', () => {
-            prepareSelect2()
+            initSelect2()
         })
 
-        function prepareSelect2(){
-            $('.select2-{{$this->id}}').select2().on('change', function(e) {
-                var data = $(this).select2("val")
+        function initSelect2() {
+            $('.select2-{{ $this->id }}').select2({
+                theme: 'bootstrap-5'
+            }).on('change', function (e) {
+                let data = $(this).val();
                 @this.select2Change(data)
-            })
+            });
         }
-    })
+    });
 </script>
